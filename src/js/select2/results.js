ce66795e04ee8f4e606e1a848354b188fec7ed28
zhouxiaoyan <609519669@qq.com>
2017-09-27 23:08:11 +0800

    添加注释

--- src/js/select2/results.js
+++ src/js/select2/results.js
@@ -12,7 +12,7 @@ define([
   }
 
   Utils.Extend(Results, Utils.Observable);
-
+//创建ul节点  并且添加class名 如果参数当中有multiple的话  就给ul在添加一个aria-multiselectable属性，最后返回这个ul节点
   Results.prototype.render = function () {
     var $results = $(
       '<ul class="select2-results__options" role="tree"></ul>'
@@ -26,32 +26,37 @@ define([
 
     return $results;
   };
-
+//清空这个ul节点下的内容
   Results.prototype.clear = function () {
     this.$results.empty();
   };
 
   Results.prototype.displayMessage = function (params) {
+    //这个参数params是什么呢？他决定了这里li中的内容。
     var escapeMarkup = this.options.get('escapeMarkup');
 
     this.clear();
     this.hideLoading();
-
+//这里创建了li 节点，并添加了class名为"select2-results__option”
     var $message = $(
       '<li role="treeitem" aria-live="assertive"' +
       ' class="select2-results__option"></li>'
     );
-
+//这里获取transition这个参数。这个参数是一个transition对象，它是Tansition函数的一个实例。tansition实例是通过Tansition._loadpath来实例化的。
+//这个静态函数在实例化之前 根据传入的语言参数（表示哪种语言的参数）去require 本地文件。require后的内容一个是保存在Transition._cache这个缓存对象中。
+    //一个是作为实例化tansition的参数,把require的内容保存到实例化对象的属性dist上，key值就是语言参数。实例化对象有个get函数就是根据语言
+    //这个可以值获取这个require的内容。这个实例的过程是在try  catch 中做的。因为如果本地没有这个文件
+//require 就会失败。所以对他进行了一个容错的处理。也就是说这个trantion对象一个是保存了某种语言文件。一是可以获取这个语言文件
     var message = this.options.get('translations').get(params.message);
-
+//通过这个语言文件和escape的处理。将内容保存在li元素中。
     $message.append(
       escapeMarkup(
         message(params.args)
       )
     );
-
+    //给这个元素添加class名。前面有添加一个名字。在此基础上又添加了。这里没有用$message.addclass的方式添加。
     $message[0].className += ' select2-results__message';
-
+//把li 添加的ul中。
     this.$results.append($message);
   };
 
@@ -63,7 +68,8 @@ define([
     this.hideLoading();
 
     var $options = [];
-
+//如果data的result不存在或者没有的话。就出发result：message事件。
+    //我猜想这里触发的就是上面的那个diaplaymessage函数。传入的参数是被包装有接口{message，args}
     if (data.results == null || data.results.length === 0) {
       if (this.$results.children().length === 0) {
         this.trigger('results:message', {
@@ -73,7 +79,8 @@ define([
 
       return;
     }
-
+//如果data存在的话  遍历这个data.results属性。每个属性值传入option 函数。option 这个函数。并把result下的每个数据都生成了一个dom 结构存入到
+    //数组$option中。最后把这个dom 数组添加到最开始创建的ul节点中。
     data.results = this.sort(data.results);
 
     for (var d = 0; d < data.results.length; d++) {
@@ -81,12 +88,12 @@ define([
 
       var $option = this.option(item);
 
-      $options.push($option);
+      $options.push($option);//循环第一层数据分别调用option 来生成dom节点。option 中也会根据children中的数据进一步调用自己来生成嵌套的节点。
     }
 
-    this.$results.append($options);
+    this.$results.append($options);//节点都添加到了ul中。
   };
-
+  //把最开始创建的ul标签插入到dropdown这个节点中。
   Results.prototype.position = function ($results, $dropdown) {
     var $resultsContainer = $dropdown.find('.select2-results');
     $resultsContainer.append($results);
@@ -119,7 +126,7 @@ define([
 
   Results.prototype.setClasses = function () {
     var self = this;
-
+//this.data是传入的dataadapter实例。它的current函数有什么作用呢？
     this.data.current(function (selected) {
       var selectedIds = $.map(selected, function (s) {
         return s.id.toString();
@@ -166,7 +173,11 @@ define([
   Results.prototype.hideLoading = function () {
     this.$results.find('.loading-results').remove();
   };
-
+//这个函数是先创建一个li节点，并且添加class名。组建arrt数据，如果是传入的数据是一般的，那么这个attr的值为
+  //{role:'treeitem',aria-selected:false},如果传入的数据有disabled属性，那么 这个attr变成{role:'treeitem',aria-disabled:true}
+  //如果没有id属性的话，那么arrt是{role:'treeitem'}。如果有child属性的话，arr改为{role:'group'}
+  //这个属性值的修改可以在最初的attr上通过delete来删除某个属性或者添加某个属性。除此之外也可以直接赋值吧  我觉得。
+  //这样处理之后开始对这个li元素进行修饰。根据不同的参数进行修饰。如果有data有title属性，那么久添加到li元素中。并把attr通过循环来添加到li的attr中。
   Results.prototype.option = function (data) {
     var option = document.createElement('li');
     option.className = 'select2-results__option';
@@ -204,7 +215,10 @@ define([
 
       option.setAttribute(attr, val);
     }
-
+//针对data有children的情况。做了特殊处理。就是创建一个strong 元素，并添加class名。调用template将数据渲染到label节点中。
+    //在就是循环data的children属性，获得数据，调用自己也就是option函数。总的来说这个函数就是根据数据来创建并包装li元素的。也就是说循环
+    //children会创建很多li标签。最后创建了一个ul标签。来包裹这些根据children下的数据生成的li标签。并且最后和label标签一起添加到父级li标签中。
+    //所以说一条数据就是一条li标签。只不过根据数据的不同。还要额外做一些东西。
     if (data.children) {
       var $option = $(option);
 
@@ -233,11 +247,12 @@ define([
       $option.append(label);
       $option.append($childrenContainer);
     } else {
-      this.template(data, option);
+      this.template(data, option);//没有子元素的话就直接渲染
     }
 
     $.data(option, 'data', data);
-
+//最后返回这个父级li标签。所以说一层层处理。先循环处理上一级dom节点。在针对下一级的某个dom节点下的子节点循环处理。当然他们有共同的地方，
+    //考虑重复利用。
     return option;
   };
 
@@ -250,7 +265,7 @@ define([
 
     container.on('results:all', function (params) {
       self.clear();
-      self.append(params.data);
+      self.append(params.data);//监听事件来调用上面定义的函数。
 
       if (container.isOpen()) {
         self.setClasses();
@@ -504,7 +519,7 @@ define([
       this.$results.scrollTop(nextOffset);
     }
   };
-
+//这个模板函数 是讲最终渲染的数据进行一个处理。根据参数templateresult进行处理。如果有结果就渲染到dom中。没有就不渲染。
   Results.prototype.template = function (result, container) {
     var template = this.options.get('templateResult');
     var escapeMarkup = this.options.get('escapeMarkup');
